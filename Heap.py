fa786cdfa695351fe13b0022235b46e0adfd68fd
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2025-11-22 23:08:50 +0700

    🖥️ Heap push&structure

---
+++ Heap.py
@@ -0,0 +1,57 @@
+class Node():
+    def __init__(self, value: int): 
+        self.value = value
+        self.full = False
+        self.leftNode = None
+        self.rightNode = None
+
+class Heap():
+    def __init__(self):
+        self.top = None
+        self.empty = True
+
+    def __newNode(self, value: int) -> Node:
+        return Node(value)
+
+    def push(self, value: int):
+        if self.empty:
+            self.top = self.__newNode(value)
+            self.empty = False
+        else:
+            self.__insert(self.top, value)
+            
+    def __insert(self, node: Node, value: int):
+        if node.leftNode == None:
+            node.leftNode = self.__newNode(value)
+            return
+        elif node.rightNode == None:
+            node.rightNode = self.__newNode(value)
+            node.full = True
+            return
+        elif node.leftNode.full == False or node.rightNode.full == True:
+            node.rightNode.full = False
+            self.__insert(node.leftNode, value)
+        elif node.leftNode.full == True:
+            self.__insert(node.rightNode, value)
+
+        if node.leftNode.full == True and node.rightNode.full == True:
+            node.full = True
+        else:
+            node.full = False
+
+    def stucture(self):
+        return self.__traversal(self.top)
+
+    def __traversal(self, node: Node) -> list:
+        resultList = [node.value]
+        if node.leftNode != None:
+            resultList.append(self.__traversal(node.leftNode))
+        if node.rightNode != None:
+            resultList.append(self.__traversal(node.rightNode))
+        return resultList
+    
+heap = Heap()
+
+for i in range(10):
+    heap.push(i)
+print(heap.stucture())
