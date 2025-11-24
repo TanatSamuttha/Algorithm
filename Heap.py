4f00905635a2b75f80bf6056f878bfa65014fa44
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2025-11-24 16:31:48 +0700

    🖥️ Add swap

--- Heap.py
+++ Heap.py
@@ -23,16 +23,33 @@ class Heap():
     def __insert(self, node: Node, value: int):
         if node.leftNode == None:
             node.leftNode = self.__newNode(value)
+            if node.leftNode.value > node.value:
+                handle = node.value
+                node.value = node.leftNode.value
+                node.leftNode.value = handle
             return
         elif node.rightNode == None:
             node.rightNode = self.__newNode(value)
             node.full = True
+            if node.rightNode.value > node.value:
+                handle = node.value
+                node.value = node.rightNode.value
+                node.rightNode.value = handle
             return
-        elif node.leftNode.full == False or node.rightNode.full == True:
+
+        if node.leftNode.full == False or node.rightNode.full == True:
             node.rightNode.full = False
             self.__insert(node.leftNode, value)
+            if node.leftNode.value > node.value:
+                handle = node.value
+                node.value = node.leftNode.value
+                node.leftNode.value = handle
         elif node.leftNode.full == True:
             self.__insert(node.rightNode, value)
+            if node.rightNode.value > node.value:
+                handle = node.value
+                node.value = node.rightNode.value
+                node.rightNode.value = handle
 
         if node.leftNode.full == True and node.rightNode.full == True:
             node.full = True
@@ -49,9 +66,10 @@ class Heap():
         if node.rightNode != None:
             resultList.append(self.__traversal(node.rightNode))
         return resultList
-    
+
 heap = Heap()
 
-for i in range(10):
+numList = [3, 1, 4, 2, 5, 6]
+for i in numList:
     heap.push(i)
-print(heap.stucture())
+    print(heap.stucture())
